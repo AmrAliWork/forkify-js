9ba329d19adb74e5c87ba48971d472b743657d5f
Amr Ali <amrali20045@gmail.com>
2025-10-19 22:03:25 +0300

    Fix Parcel version and public-url to show icons correctly

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,5 +1,5 @@
-import View from './View.js';
 import icons from 'url:../../img/icons.svg';
+import View from './View.js';
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
   _message = 'Recipe was successffuly uploaded';
